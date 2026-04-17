e1ef77ebfca634a35062f2d200ded5d926b3722f
chris7716 <hkraclh@gmail.com>
2026-04-17 20:31:17 +0530

    Add testing script

--- test_graph_encoding.sh
+++ test_graph_encoding.sh
@@ -53,9 +53,9 @@ set -x
 "$VG" index -x chr22.xg chr22.vg
 "$VG" prune -k 16 chr22.vg > chr22.pruned.vg
 "$VG" index -g chr22.gcsa -k 16 -b "$TMPDIR" chr22.pruned.vg
-set +x
 "$VG" map -f chr22_reads.fastq -x chr22.xg -g chr22.gcsa -t 8 > chr22_aln.gam
 "$VG" convert -F chr22_aln.gam > chr22_aln.gaf
+set +x
 
 echo "==> [8/9] Encoding BAM to graph-encoded GBAM..."
 "$GBAM_BINARY" \
