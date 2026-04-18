00888171978c697f9fa3a68eb2caf0f617f62f2c
chris7716 <hkraclh@gmail.com>
2026-04-18 13:59:40 +0530

    Update script

--- test_graph_encoding.sh
+++ test_graph_encoding.sh
@@ -9,53 +9,53 @@ WORKDIR="/home/hasitha/data/projects/gbam/graph_test"
 mkdir -p "$WORKDIR"
 cd "$WORKDIR"
 
-echo "==> [1/9] Downloading hg19 reference..."
-if [ ! -f hg19.fa ]; then
-    wget -c "https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz"
-    gunzip -f hg19.fa.gz
-    samtools faidx hg19.fa
-else
-    echo "    hg19.fa already exists, skipping download"
-fi
+# echo "==> [1/9] Downloading hg19 reference..."
+# if [ ! -f hg19.fa ]; then
+#     wget -c "https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz"
+#     gunzip -f hg19.fa.gz
+#     samtools faidx hg19.fa
+# else
+#     echo "    hg19.fa already exists, skipping download"
+# fi
 
-echo "==> [2/9] Extracting chr22..."
-samtools faidx hg19.fa chr22 > chr22.fa
-samtools faidx chr22.fa
+# echo "==> [2/9] Extracting chr22..."
+# samtools faidx hg19.fa chr22 > chr22.fa
+# samtools faidx chr22.fa
 
-echo "==> [3/9] Downloading 1000 Genomes chr22 VCF..."
-wget -c "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz"
-wget -c "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz.tbi"
-VCF="ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz"
+# echo "==> [3/9] Downloading 1000 Genomes chr22 VCF..."
+# wget -c "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz"
+# wget -c "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz.tbi"
+# VCF="ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz"
 
-echo "==> [4/9] Building variation graph..."
-"$VG" construct -r chr22.fa -v "$VCF" -R chr22 -t 8 > chr22.vg
+# echo "==> [4/9] Building variation graph..."
+# "$VG" construct -r chr22.fa -v "$VCF" -R chr22 -t 8 > chr22.vg
 
-echo "==> [5/9] Exporting graph to GFA..."
-"$VG" convert -f chr22.vg > chr22.gfa
+# echo "==> [5/9] Exporting graph to GFA..."
+# "$VG" convert -f chr22.vg > chr22.gfa
 
-echo "==> [6/9] Extracting chr22 reads from little.bam and converting to FASTQ..."
-samtools index "$LITTLE_BAM"
-samtools view -b "$LITTLE_BAM" chr22 > chr22_reads.bam
-READ_COUNT=$(samtools view -c chr22_reads.bam)
-echo "    chr22 reads: $READ_COUNT"
-if [ "$READ_COUNT" -eq 0 ]; then
-    echo "    WARNING: no chr22 reads in little.bam, using all reads instead"
-    cp "$LITTLE_BAM" chr22_reads.bam
-fi
-samtools fastq chr22_reads.bam > chr22_reads.fastq
+# echo "==> [6/9] Extracting chr22 reads from little.bam and converting to FASTQ..."
+# samtools index "$LITTLE_BAM"
+# samtools view -b "$LITTLE_BAM" chr22 > chr22_reads.bam
+# READ_COUNT=$(samtools view -c chr22_reads.bam)
+# echo "    chr22 reads: $READ_COUNT"
+# if [ "$READ_COUNT" -eq 0 ]; then
+#     echo "    WARNING: no chr22 reads in little.bam, using all reads instead"
+#     cp "$LITTLE_BAM" chr22_reads.bam
+# fi
+# samtools fastq chr22_reads.bam > chr22_reads.fastq
 
-echo "==> [7/9] Indexing graph and aligning reads with vg..."
-export TMPDIR="$WORKDIR/tmp"
-mkdir -p "$TMPDIR"
-echo "    vg version: $("$VG" version 2>&1 | head -1)"
-echo "    chr22.vg size: $(wc -c < chr22.vg) bytes"
-set -x
-"$VG" index -x chr22.xg chr22.vg
-"$VG" prune -k 16 chr22.vg > chr22.pruned.vg
-"$VG" index -g chr22.gcsa -k 16 -b "$TMPDIR" chr22.pruned.vg
-"$VG" map -f chr22_reads.fastq -x chr22.xg -g chr22.gcsa -t 8 > chr22_aln.gam
-"$VG" convert chr22.xg -G chr22_aln.gam > chr22_aln.gaf
-set +x
+# echo "==> [7/9] Indexing graph and aligning reads with vg..."
+# export TMPDIR="$WORKDIR/tmp"
+# mkdir -p "$TMPDIR"
+# echo "    vg version: $("$VG" version 2>&1 | head -1)"
+# echo "    chr22.vg size: $(wc -c < chr22.vg) bytes"
+# set -x
+# "$VG" index -x chr22.xg chr22.vg
+# "$VG" prune -k 16 chr22.vg > chr22.pruned.vg
+# "$VG" index -g chr22.gcsa -k 16 -b "$TMPDIR" chr22.pruned.vg
+# "$VG" map -f chr22_reads.fastq -x chr22.xg -g chr22.gcsa -t 8 > chr22_aln.gam
+# "$VG" convert chr22.xg -G chr22_aln.gam > chr22_aln.gaf
+# set +x
 
 echo "==> [8/9] Encoding BAM to graph-encoded GBAM..."
 "$GBAM_BINARY" \
